e7a4bff2886c5043a4631447b26f2bc7a9492be4
Nico550363 <y5ut010@gmail.com>
2020-02-24 16:49:24 +0900

    login実装

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
 
-  # before_action :move_to_index, except: :index
-
-  # protect_from_forgery with: :null_session
+  before_action :move_to_index, except: :index
 
   def index
     @pages = Page.limit(4).order('created_at DESC')
@@ -21,7 +19,7 @@ class PagesController < ApplicationController
     params.require(:page).permit(:name, :text, :image)
   end
   
-  # def move_to_index
-  #   redirect_to action: :index unless user_signed_in?
-  # end
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
