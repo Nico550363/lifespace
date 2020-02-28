e626674a7a645ec455a1ba879c7d62ec3c0bb1f7
Nico550363 <y5ut010@gmail.com>
2020-02-29 01:57:36 +0900

    destroy2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,11 @@ class PagesController < ApplicationController
     Page.create(name: page_params[:name], image: page_params[:image], text: page_params[:text], user_id: current_user.id)
   end
 
+  def destroy
+    page = Page.find(params[:id])
+    page.destroy if page.user_id == current_user.id
+  end
+
   private
   def page_params
     params.require(:page).permit(:name, :text, :image)
