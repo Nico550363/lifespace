237b9876c58458a0c872af1f5d0f2cd3e856cb85
Nico550363 <y5ut010@gmail.com>
2020-03-08 00:22:46 +0900

    favorite2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    @favorite = current_user.favorites.create(page_id: params[:blog_id])
+    @favorite = current_user.favorites.create(page_id: params[:page_id])
     @page = Page.find(params[:page_id])
-    render 'index.js.erb'
+    render 'index.js.haml'
   end
   
   def destroy
     @favorite = current_user.favorites.find_by(page_id: params[:id].to_i).destroy
     @page = Page.find(params[:id])
-    render 'index.js.erb'
+    render 'index.js.haml'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :pages
   has_many :comments
   has_many :favorites, dependent: :destroy
-  has_many :favorite_blogs, through: :favorites, source: :blog
+  has_many :favorite_pages, through: :favorites, source: :page
 end
