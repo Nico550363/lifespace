ac9b77f23cda86ae1c9b0a0c0b563d55acc516fa
Nico550363 <y5ut010@gmail.com>
2020-03-16 21:47:39 +0900

    変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,8 +17,7 @@ CarrierWave.configure do |config|
     }
 
     config.fog_directory  = 'lifespace2020'
-    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/lifespace2020'
-    config.asset_host = 'https://lifespace2020.s3.amazonaws.com'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/lifespace2020'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.eanble_processing = false if Rails.env.test? #test:処理をスキップ
