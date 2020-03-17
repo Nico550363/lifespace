208afa129f2ee8136da4e47cde500b5f231b91a6
Nico550363 <y5ut010@gmail.com>
2020-03-17 17:17:01 +0900

    form_with変更

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,18 +28,10 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
   end
 
-  # def update
-  #   page = Page.find(params[:id])
-  #   if page.user_id == current_user.id
-  #     page.update(update_page_params)
-  #   else 
-  #     render :edit
-  #   end
-  # end
-
   def update
-    if @page.update(update_page_params)
-      redirect_to root_path(@page)
+    page = Page.find(params[:id])
+    if page.user_id == current_user.id
+      page.update(update_page_params)
     else
       render :edit
     end
@@ -55,7 +47,7 @@ class PagesController < ApplicationController
   end
 
   def update_page_params
-    params.permit(:name, :text, :image, :style)
+    params.require(:page).permit(:name, :text, :image, :style)
   end
   
   def move_to_index
